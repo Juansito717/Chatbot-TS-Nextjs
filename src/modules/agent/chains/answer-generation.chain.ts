f399481fca5b8ea21044834683afa37752034653
Juansito717 <juanmacarrillo00@gmail.com>
2024-03-17 15:49:46 -0300

    Until memory

--- src/modules/agent/chains/answer-generation.chain.ts
+++ src/modules/agent/chains/answer-generation.chain.ts
@@ -2,6 +2,7 @@ import { StringOutputParser } from "@langchain/core/output_parsers";
 import { PromptTemplate } from "@langchain/core/prompts";
 import { RunnableSequence } from "@langchain/core/runnables";
 import { BaseLanguageModel } from "langchain/base_language";
+import { ChatOpenAI } from "@langchain/openai";
 
 // tag::interface[]
 export interface GenerateAnswerInput {
@@ -15,21 +16,38 @@ export default function initGenerateAnswerChain(
   llm: BaseLanguageModel
 ): RunnableSequence<GenerateAnswerInput, string> {
   // TODO: Create a Prompt Template
-  // const answerQuestionPrompt = PromptTemplate.fromTemplate(`
+   const answerQuestionPrompt = PromptTemplate.fromTemplate(`
+    Use only the following context to answer the following question.
+
+    Question:
+    {question}
+
+    Context:
+    {context}
+
+    Answer as if you have been asked the original question.
+    Do not use your pre-trained knowledge.
+
+    If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    Include links and sources where possible.
+    `);
   // TODO: Return a RunnableSequence
-  // return RunnableSequence.from<GenerateAnswerInput, string>([])
+    return RunnableSequence.from<GenerateAnswerInput, string>([
+      answerQuestionPrompt,
+      llm,
+      new StringOutputParser(),
+    ]);
 }
 // end::function[]
 
-/**
- * How to use this chain in your application:
+/* How to use this chain in your application:
 
 // tag::usage[]
-const llm = new OpenAI() // Or the LLM of your choice
+const llm = new ChatOpenAI() // Or the LLM of your choice
 const answerChain = initGenerateAnswerChain(llm)
 
 const output = await answerChain.invoke({
-  input: 'Who is the CEO of Neo4j?',
+  question: 'Who is the CEO of Neo4j?',
   context: 'Neo4j CEO: Emil Eifrem',
 }) // Emil Eifrem is the CEO of Neo4j
 // end::usage[]
